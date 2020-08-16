51694044eef887eb45b0b83eba95e9835f2c4595
Simon Pham <simonpham.dn@gmail.com>
2020-08-16 23:09:16 +0700

    feat: add number format extension

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -4,9 +4,16 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 extension ColorExtension on Color {
   String toHex() {
     return "#${this.value.toRadixString(16).padLeft(8, '0')}";
+  }
+}
+
+extension NumExtension on num {
+  String get formatted {
+    return NumberFormat('#,###').format(this);
   }
 }
